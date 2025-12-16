7d06cbd11c44d37c5aede42d50eb2c69539bf494
Dhirendra Kumar Mishra <dkm00125@gmail.com>
2025-12-16 12:12:01 +0530

    ✅ Completed
    Next.js project setup
    
    Docker + PostgreSQL
    
    Prisma config (v7 compatible)
    
    Schema design
    
    Migrations
    
    Prisma Studio verification

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+// prisma.config.ts
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    // read DATABASE_URL from your .env
+    url: env("DATABASE_URL"),
+  },
+});
